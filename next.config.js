18a3615364aeed14f3cd44b11e20d1f437f55543
nikshitsehgal <nikshgl16@gmail.com>
2021-08-07 19:50:55 +0530

    mapint

--- next.config.js
+++ next.config.js
@@ -2,4 +2,8 @@ module.exports = {
   images: {
     domains: ["links.papareact.com", "a0.muscache.com"],
   },
+  env: {
+    mapbox_key:
+      "pk.eyJ1Ijoibmlrc2hpdDEiLCJhIjoiY2tzMWd1eGl0MWNkMzJwbzk0emIzM2pzbyJ9.KgdnalmNexMLdZuvtiYNaQ",
+  },
 };
--- pages/search.js
+++ pages/search.js
@@ -3,6 +3,7 @@ import Footer from "../components/Footer";
 import { useRouter } from "next/dist/client/router";
 import { format } from "date-fns";
 import InfoCard from "../components/InfoCard";
+import Map from "../components/map";
 
 function search({ searchResults }) {
   const router = useRouter();
@@ -48,6 +49,10 @@ function search({ searchResults }) {
               )
             )}
           </div>
+        </section>
+
+        <section className="xs:flex xs:flex-col xl:inline-flex xl:min-w-[600px]">
+          <Map searchResults={searchResults} />
         </section>
       </main>
 
